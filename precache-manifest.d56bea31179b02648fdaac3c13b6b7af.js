3941d6b225ac2dc6926b89132d1787688bb32356
atomr <atomr@MacBook-Air-atomr.local>
2020-04-25 16:56:27 +0500

    Updates

---
+++ precache-manifest.d56bea31179b02648fdaac3c13b6b7af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5ce9b691fd76ce7e3033c77d448c6a",
+    "url": "/react-github-users/index.html"
+  },
+  {
+    "revision": "7bcc86b2def1f9b5d263",
+    "url": "/react-github-users/static/css/main.c3f6c43d.chunk.css"
+  },
+  {
+    "revision": "6c58e296d6a47198c4dd",
+    "url": "/react-github-users/static/js/2.74904056.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-github-users/static/js/2.74904056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bcc86b2def1f9b5d263",
+    "url": "/react-github-users/static/js/main.a78a1ad6.chunk.js"
+  },
+  {
+    "revision": "f49935a2d9722c8aabf2",
+    "url": "/react-github-users/static/js/runtime-main.b834a216.js"
+  }
+]);
